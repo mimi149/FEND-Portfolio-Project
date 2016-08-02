dd485187a930e4e72ed4551c2d3b4bc92ab0bee6
baotran <baotranmimi@gmail.com>
2016-08-02 11:33:58 -0700

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+module.exports = function (grunt) {
+	require("load-grunt-tasks")(grunt);
+	var config = grunt.file.readYAML("Gruntconfig.yml");
+
+	grunt.initConfig({
+		responsive_images: {
+			dev: {
+				options: {
+					engine: "gm",
+					sizes: [{
+							width: 320,
+							quality: 20
+						},
+						{
+							width: 640,
+							quality: 30
+						},
+						{
+							width: 1024,
+							quality: 30
+						},
+						{
+							width: 1600,
+							quality: 20
+						}
+					]
+				},
+				files: [{
+					expand: true,
+					src: ["*.{jpg,gif,png}", "!udacity_logo.png"],
+					cwd: config.imgSrcDir,
+					dest: config.imgDir
+				}]
+			}
+		},
+
+		/* Copy the "fixed" images that don"t go through processing into the images directory */
+		copy: {
+		  dev: {
+			files: [{
+			  expand: false,
+			  src: config.imgSrcDir + "udacity_logo.png",
+			  dest: config.imgDir + "udacity_logo.png"
+			}]
+		  }
+		},
+
+		lint5: {
+			dirPath: config.src,
+			defaults: {
+				"email": "a@a.com",
+				"username": "abcd"
+			},
+			templates: [
+				"index.html"
+			],
+			ignoreList: []
+		},
+
+		htmlhint: {
+			html1: {
+				options: {
+					"tag-pair": true
+				},
+				src: [config.src + "*.html"]
+			},
+			html2: {
+				options: {
+					"tag-pair": true
+				},
+				src: [config.src + "*.html"]
+			}
+		},
+
+		csslint: {
+			strict: {
+				options: {
+					import: 2
+				},
+				src: [config.cssSrcDir + "*.css", "!" + config.cssSrcDir + "*.min.css"]
+			},
+			lax: {
+				options: {
+					import: false
+				},
+				src: [config.cssSrcDir + "style.css"]
+			}
+		},
+
+		prettyugly: {
+			options: {},
+			minify_separate: {
+				expand: true,
+				cwd: config.cssSrcDir,
+				src: ["*.css", "!*.min.css"],
+				dest: config.cssDir,
+				ext: ".min.css",
+				extDot: "first"
+			}
+		},
+
+		jshint: {
+			options: {
+				"eqeqeq": true
+			},
+			all: [
+				"Gruntfile.js",
+				config.jsSrcDir + "*.js",
+				"!" + config.jsSrcDir + "bootstrap.min.js",
+				"!" + config.jsSrcDir + "jquery.min.js"
+			]
+		}
+	});
+
+	grunt.registerTask("default", [
+		"lint5",
+		"htmlhint",
+		"csslint",
+		"prettyugly",
+		"jshint",
+		"responsive_images"
+	]);
+
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# FEND-Portfolio-Project
+
+Portfolio Project for Udacity's Front End Nanodegree Program
+
+This is a static portfolio page based on these courses:
+* [Responsive Web Design Fundamentals](https://www.udacity.com/course/responsive-web-design-fundamentals--ud893)
+* [Responsive Images](https://www.udacity.com/course/responsive-images--ud882)
+
+Automation, performing repetitive tasks with Grunt JavaScript Task Runner.
